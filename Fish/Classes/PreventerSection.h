60064370d0646499c69ac086ce0380f197cf45be
Billy-Jo Hunia <bjh31@dhcp197.resnet.scms.waikato.ac.nz>
2011-09-15 14:46:28 +1200

    15/09/2011 Billy Jo

---
+++ Fish/Classes/PreventerSection.h
@@ -0,0 +1,22 @@
+//
+//  PreventerSection.h
+//  Fish
+//
+//  Created by Billy-Jo Hunia on 6/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DustMite.h"
+
+@interface PreventerSection : NSObject {
+	 NSMutableArray *sections;
+}
+
+-(void)createSection:(float)xStart :(float)yStart :(bool)isOnLeftSide;
+-(void)checkCollisionWithTrigger: (DustMite *) mite;
+//-(void)checkHealthOfSection;
+-(void)repairSection;
+-(NSMutableArray *)getSection;
+
+@end
